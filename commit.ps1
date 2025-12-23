c75818977692ff84ff70aa55c626185987303ef2
haizi <thehub@iziizz.slmail.me>
2025-12-23 20:44:15 +0800

    fix: update directory path in SSH command for pulling changes

--- commit.ps1
+++ commit.ps1
@@ -2,4 +2,4 @@ npm run build
 git add .
 git commit -m "$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")"
 git push
-ssh haizi@139.162.55.58 "cd imagen-ai/ && git pull"
+ssh haizi@139.162.55.58 "cd imagen-homepage/ && git pull"
